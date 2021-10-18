207f62160c58e053e0c90e4b50d9552001876f32
Mbhekeni <mbhekenim.mathe@gmail.com>
2021-10-19 01:31:43 +0200

    first commit

--- Documents/Personal/HyperionDev/LEVEL 2/Task 6/helloWorld.js
+++
@@ -1,7 +0,0 @@
-function Submit(){
-
-    let name = document.getElementById("Fname").value;
-    let surname = document.getElementById("Lname").value;
-
-    document.getElementById("display").innerHTML = name +" "+ surname;
-}Submit();
